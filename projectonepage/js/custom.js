2d7d6a71c2fb097f340d3d89aa428846e8c1320d
Rezone Prince <97110052+rezone510@users.noreply.github.com>
2022-02-09 07:10:32 +0600

    Add files via upload

---
+++ projectonepage/js/custom.js
@@ -0,0 +1,15 @@
+$('.slider_main').slick({
+	autoplay: true,
+	arrows: false,
+	dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+});
+
+$('.banner-slider').slick({
+	autoplay: true,
+	arrows: false,
+	dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+});
